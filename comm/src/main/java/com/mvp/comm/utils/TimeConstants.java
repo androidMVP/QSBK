9ca1936351204100d001a64b00539a4aa6b967ea
androidMVP <wujialinzuishuai@gmail.com>
2018-06-14 18:21:48 +0800

    添加图片

---
+++ comm/src/main/java/com/mvp/comm/utils/TimeConstants.java
@@ -0,0 +1,22 @@
+package com.mvp.comm.utils;
+import android.support.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+/**
+ * @author MVP
+ *         created: 2018/6/14
+ *         desc:
+ */
+public class TimeConstants {
+	public static final int MSEC = 1;
+	public static final int SEC  = 1000;
+	public static final int MIN  = 60000;
+	public static final int HOUR = 3600000;
+	public static final int DAY  = 86400000;
+
+	@IntDef({MSEC, SEC, MIN, HOUR, DAY})
+	@Retention(RetentionPolicy.SOURCE)
+	public @interface Unit {
+	}
+}
